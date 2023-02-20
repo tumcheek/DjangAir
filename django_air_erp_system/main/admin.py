dcf0d2562fc668d0f31932827aba465cc6171732
Danil Tumanov <dania.tumanov@gmail.com>
2023-02-20 14:14:52 +0200

    regist all models

--- django_air_erp_system/main/admin.py
+++ django_air_erp_system/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(FlightModel)
+admin.site.register(PriceModel)
+admin.site.register(AirplaneModel)
+admin.site.register(SeatModel)
+admin.site.register(SeatTypeModel)
+admin.site.register(TicketModel)
+admin.site.register(PassengerModel)
+admin.site.register(OptionModel)
+admin.site.register(LuggageModel)
