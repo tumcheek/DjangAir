d03494d81c7d95f57678f85c323541409b54bff4
Danil Tumanov <dania.tumanov@gmail.com>
2023-03-05 11:13:39 +0200

    create IndexView

--- django_air_erp_system/main/views.py
+++ django_air_erp_system/main/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
 
-# Create your views here.
+from .forms import FlightForm
+
+
+class IndexView(View):
+    template_name = 'main/index.html'
+
+    def get(self, request):
+        form = FlightForm()
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = FlightForm(request.POST)
+        if form.is_valid():
+            query_info = {
+                'start_location': form.cleaned_data['start_location'],
+                'end_location': form.cleaned_data['end_location'],
+                'start_date': form.cleaned_data['start_date'],
+                'passenger_number': form.cleaned_data['passenger_number']
+            }
+            return redirect(reverse('main:search_result', kwargs=query_info))
+
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context)
