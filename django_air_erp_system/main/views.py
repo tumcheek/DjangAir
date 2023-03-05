0344dbe50e0f2ab8f7ebe68f2015f1ea5ef1c27f
Danil Tumanov <dania.tumanov@gmail.com>
2023-03-05 11:39:58 +0200

    create get_flights func

--- django_air_erp_system/main/views.py
+++ django_air_erp_system/main/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
+from django.views.decorators.http import require_http_methods
 
 from .forms import FlightForm
+from .models import FlightModel
+from .utils import get_flights_info
+
+
+@require_http_methods(["GET"])
+def get_flights(request, start_location, end_location, start_date, passenger_number):
+    flights_query = FlightModel.objects.filter(
+        start_location=start_location,
+        end_location=end_location,
+        start_date=start_date
+    )
+    flights = get_flights_info(flights_query, passenger_number)
+    is_flights = False if not flights else True
+
+    context = {
+        'flights': flights,
+        'is_flights': is_flights
+    }
+    return render(request, 'main/flights_result.html', context)
 
 
 class IndexView(View):
