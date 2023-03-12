fd1ab0eb045bf6fd7e620ed96befbcb41ad1fab2
Danil Tumanov <dania.tumanov@gmail.com>
2023-03-12 11:35:31 +0200

    add EmailSubjectModel

--- django_air_erp_system/main/models.py
+++ django_air_erp_system/main/models.py
@@ -114,3 +114,11 @@ class LuggageModel(models.Model):
 
     def __str__(self):
         return str(self.ticket.passenger)
+
+
+class EmailSubjectModel(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    subject = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
