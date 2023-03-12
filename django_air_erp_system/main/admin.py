7765cfc1908c816840709b78911f0b29a03ce55c
Danil Tumanov <dania.tumanov@gmail.com>
2023-03-12 11:37:24 +0200

    regist EmailSubjectModel

--- django_air_erp_system/main/admin.py
+++ django_air_erp_system/main/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from .models import *
+from . import models
 
 # Register your models here.
-admin.site.register(FlightModel)
-admin.site.register(PriceModel)
-admin.site.register(AirplaneModel)
-admin.site.register(SeatModel)
-admin.site.register(SeatTypeModel)
-admin.site.register(TicketModel)
-admin.site.register(PassengerModel)
-admin.site.register(OptionModel)
-admin.site.register(LuggageModel)
+admin.site.register(models.FlightModel)
+admin.site.register(models.PriceModel)
+admin.site.register(models.AirplaneModel)
+admin.site.register(models.SeatModel)
+admin.site.register(models.SeatTypeModel)
+admin.site.register(models.TicketModel)
+admin.site.register(models.PassengerModel)
+admin.site.register(models.OptionModel)
+admin.site.register(models.LuggageModel)
+admin.site.register(models.EmailSubjectModel)
