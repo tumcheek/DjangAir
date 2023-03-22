07a6bda2dedb67827056e8f7a98963e07748c461
Danil Tumanov <dania.tumanov@gmail.com>
2023-03-22 11:13:05 +0200

    add UserLoginForm

--- django_air_erp_system/main/forms.py
+++ django_air_erp_system/main/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class SearchFlightForm(forms.Form):
@@ -6,3 +7,19 @@ class SearchFlightForm(forms.Form):
     end_location = forms.CharField(max_length=255)
     start_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
     passenger_number = forms.IntegerField()
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = forms.EmailField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Email', 'id': 'id_username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'id': 'id_password',
+        }
+        )
+    )
