aeeb25c4b1d37cc472d94e3c79049c7d8fdd48f3
Danil Tumanov <dania.tumanov@gmail.com>
2023-05-18 13:29:52 +0300

    add URLs for authentication app

--- django_air_erp_system/django_air_erp_system/urls.py
+++ django_air_erp_system/django_air_erp_system/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('social_django.urls', namespace='social')),
+    path('auth/', include('authentication.urls')),
     path('', include('main.urls')),
 ]
 
