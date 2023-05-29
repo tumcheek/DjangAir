cb122eca0d78aff51d4ea9d121fb0eed99164584
Danil Tumanov <dania.tumanov@gmail.com>
2023-05-29 14:25:53 +0300

    add URLs for staff authorization

--- django_air_erp_system/authentication/urls.py
+++ django_air_erp_system/authentication/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
         path('login/', views.LoginView.as_view(), name='login'),
         path('logout/', LogoutView.as_view(), name='logout'),
         path('registration/', views.RegistrationView.as_view(), name='registration'),
+        path('staff/login/', views.StaffLoginView.as_view(), name='staff-login'),
+        path('staff/logout/', views.staff_logout_view, name='staff-logout'),
 ]
